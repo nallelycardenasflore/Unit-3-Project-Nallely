12b99d59d9c5189c379f759cbb6a5cf4fb9487a3
nallelycardenasflore <123699626+nallelycardenasflore@users.noreply.github.com>
2023-01-26 14:08:24 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let StoryPart2Save = document.querySelector(".option-one-screen");
+let StoryPart2Defeat = document.querySelector(".option-two-screen");
+let StoryPart3Save = document.querySelector(".option-one-end");
+let StoryPart3Defeat = document.querySelector(".option-two-end");
+let OpeningMessage = document.querySelector(".message");
+let Question = document.querySelector(".question");
+let RunveryfastButton = document.querySelector(".option-one");
+let HidebehindatreeButton = document.querySelector(".option-two");
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+
+
+
+RunveryfastButton.onclick=function(){
+StoryPart2Save.style.display="block";
+};
+
+
+
+
+HidebehindatreeButton.onclick=function(){
+StoryPart2Defeat.style.display="block";
+};
+
+//
+
+
